454bbf3f45a34effc6c99c6d327ac153c127f102
Rafael Ferreira <rafaelfca.ferreira@gmail.com>
2020-06-12 17:48:25 -0300

    Created xcode project and added .gitignore file

---
+++ HackatonCCR/HackatonCCR/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HackatonCCR
+//
+//  Created by Rafael Ferreira on 12/06/20.
+//  Copyright © 2020 Rafael Ferreira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
