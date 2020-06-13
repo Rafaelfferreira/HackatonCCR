6c613889e7c3c35596b73c25a8983d4a0d6d20d2
Rafael Ferreira <rafaelfca.ferreira@gmail.com>
2020-06-13 09:43:01 -0300

    Integrated custom map markers

--- HackatonCCR/HackatonCCR/MapVC.swift
+++ HackatonCCR/HackatonCCR/MapVC.swift
@@ -16,6 +16,9 @@ class MapVC: UIViewController {
     @IBOutlet weak var mapView: GMSMapView!
     
     let locationManager = CLLocationManager();
+    var currentMarkers: [GMSMarker] = [];
+    
+    var markerSize: Int = 35;
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,10 +30,14 @@ class MapVC: UIViewController {
         mapView.settings.myLocationButton = true;
         mapView.isMyLocationEnabled = true
         
-        
+        // Inicializando o aplicativo na posicao atual do usuario
         getCurrentLocation();
+        // Setta o frame para comecar na posicao do usuario
         let camera = GMSCameraPosition.camera(withLatitude: (locationManager.location?.coordinate.latitude)!, longitude: (locationManager.location?.coordinate.longitude)!, zoom: 15);
         mapView.camera = camera;
+        
+        //testando adicionar os markers
+        addMarkerToMap(latitude: (locationManager.location?.coordinate.latitude)! - 0.003, longitude: (locationManager.location?.coordinate.longitude)!, markerText: "Teste")
     }
     
     func getCurrentLocation() {
@@ -44,12 +51,31 @@ class MapVC: UIViewController {
             locationManager.startUpdatingLocation();
         }
     }
+    
+    func addMarkerToMap(latitude: CLLocationDegrees, longitude: CLLocationDegrees, markerText: String) {
+        let marker = GMSMarker(position: CLLocationCoordinate2D(latitude: latitude, longitude: longitude))
+        marker.title = markerText
+        marker.icon = UIImage(named: "MarkerIcon")
+        marker.setIconSize(scaledToSize: CGSize(width: markerSize, height: markerSize * 1.25))
+        marker.map = mapView
+        
+        currentMarkers.append(marker)
+    }
 }
 
 extension MapVC: CLLocationManagerDelegate {
-    // Esse metodo eh chamado sempre que a localizacao do usuario se atualiza na acuracia definida acima
+    // MARK: - Metodo chamado sempre que a localizacao do usuario mudar
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         guard let locValue: CLLocationCoordinate2D = manager.location?.coordinate else { return }
-        print("locations = \(locValue.latitude) \(locValue.longitude)")
+    }
+}
+
+extension GMSMarker {
+    func setIconSize(scaledToSize newSize: CGSize) {
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 0.0)
+        icon?.draw(in: CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height))
+        let newImage: UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        icon = newImage
     }
 }
