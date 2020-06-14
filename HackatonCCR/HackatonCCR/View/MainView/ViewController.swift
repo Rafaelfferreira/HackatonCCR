06c293289cc0a265664539186eff91873039a7f6
meduardcn <m.eduardacn@gmail.com>
2020-06-14 16:14:28 -0300

    merge (?)

--- HackatonCCR/HackatonCCR/View/MainView/ViewController.swift
+++ HackatonCCR/HackatonCCR/View/MainView/ViewController.swift
@@ -32,7 +32,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        getPlacesInformations()
+//        getPlacesInformations()
     }
     
     //popula tableView
