88dbe45c6930718842a657286a30c558d3af0729
colombelli <xcolombelli@gmail.com>
2020-06-14 21:20:34 -0300

    query function to get all nearby places added; i couldnt test cause my linter bugged yet again

--- cloudFunctions/functions/src/index.ts
+++ cloudFunctions/functions/src/index.ts
@@ -6,8 +6,6 @@ admin.initializeApp(functions.config().firebase)
 
 export const getPlacePercentageReviews = functions.https.onRequest((request, response) => {
 
-
-
     function distance(lat1:number, lon1:number, lat2:number, lon2:number, unit:string) {
         var radlat1 = Math.PI * lat1/180
         var radlat2 = Math.PI * lat2/180
@@ -93,4 +91,110 @@ export const getPlacePercentageReviews = functions.https.onRequest((request, res
                 response.status(500).send(error)
         })
 })
+
+
+
+export const getPlacePercentageReviewsUfs = functions.https.onRequest((request, response) => {
+
+    function distance(lat1:number, lon1:number, lat2:number, lon2:number, unit:string) {
+        var radlat1 = Math.PI * lat1/180
+        var radlat2 = Math.PI * lat2/180
+        var theta = lon1-lon2
+        var radtheta = Math.PI * theta/180
+        var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+        if (dist > 1) {
+            dist = 1;
+        }
+        dist = Math.acos(dist)
+        dist = dist * 180/Math.PI
+        dist = dist * 60 * 1.1515
+        if (unit=="K") { dist = dist * 1.609344 }
+        if (unit=="N") { dist = dist * 0.8684 }
+        return dist
+    }
+
+
     
+    let lat = request.query.lat
+    let lng = request.query.lng
+    let ufs = request.query.ufs
+    let qtd = request.query.qtd
+
+    let lat_num = Number(lat)
+    let lng_num = Number(lng)
+    let ufs_str = String(ufs).split('-')
+    let qtd_num = Number(qtd)
+
+
+    var all_data = Array()
+    async function grabAllData(): Promise<void> {
+        
+        for (const uf of ufs_str){
+           admin.firestore().collection("placesOpt").doc(uf).collection(uf).get()
+                .then(snapshot => {
+                        let data = snapshot.docs.map(doc => doc.data())
+                        all_data = all_data.concat(data)
+                }) .catch(error => {
+                    response.status(500).send(error)
+            })
+    } 
+
+    async function wrapperAwait() {
+        await grabAllData()
+    }
+
+    wrapperAwait()
+
+                if (qtd_num > all_data.length) {
+                    qtd_num = all_data.length - 1
+                }
+
+                
+
+                let closest = []
+                for (const place of all_data.slice(0,qtd_num)) {
+                    closest.push({
+                    "dist": distance(lat_num, lng_num, 
+                        place.coordinates._latitude,
+                        place.coordinates._longitude, "K"),
+                    "place": place
+                    })
+                }
+                
+                closest.sort((a, b) => a.dist < b.dist ? -1 : a.dist > b.dist ? 1 : 0)
+
+                for (const place of all_data.slice(qtd_num,)) {
+                    let curr_dist =  distance(lat_num, lng_num, place.coordinates._latitude,                            place.coordinates._longitude, "K")
+                  
+                    let control=true
+                    closest = closest.map(item => {
+                      if (item.dist > curr_dist && control){
+                        control = false
+                        return {"dist": curr_dist, "place": place}
+                      } else {
+                        return item
+                      }
+                    })
+                }
+                
+
+                let response_obj = []
+                for (const place of closest) {
+                    response_obj.push(place.place)
+                }
+            
+        response.send(response_obj)
+
+
+        }
+})
+
+
+
+
+export const getFretagem = functions.https.onRequest((request, response) => {
+
+    // reference: https://www.tabelasdefrete.com.br/p/calculo-carreteiro
+    
+    response.send('a')
+})
